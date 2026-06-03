d60f72c674fc3c34acfd4d25047b5eead48c7f45
0m.ax <git@0m.ax>
2026-06-03 17:46:53 +0200

    gst

--- flake.nix
+++ flake.nix
@@ -37,7 +37,22 @@
         };
 
         devShells.default = pkgs.mkShell {
-          packages = [ obs pkgs.srtrelay ];
+          packages = [
+            obs
+            pkgs.srtrelay
+            pkgs.gst_all_1.gstreamer
+            pkgs.gst_all_1.gst-plugins-base
+            pkgs.gst_all_1.gst-plugins-good
+            pkgs.gst_all_1.gst-plugins-bad
+            pkgs.gst_all_1.gst-plugins-ugly
+            pkgs.gst_all_1.gst-libav
+          ];
+
+          shellHook = ''
+            export GST_PLUGIN_PATH="${pkgs.lib.makeSearchPath "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
+              gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav
+            ])}"
+          '';
         };
       });
 }
